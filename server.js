adb6e8fc294430cca2a90d53e48ad09320d321d2
Rounak <riz.moj.msd97@gmail.com>
2022-07-10 22:21:47 +0530

    initial

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 2345;
+const connect = require("./config/db")
+
+
+app.use(express.json());
+
+
+app.use("/short", require("./routes/urlShortener"))
+
+app.listen(port, (req,res)=>{
+    connect()
+    console.log(`listening to port ${port}`)
+})
