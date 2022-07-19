f91e8c29e22a0fee069f4b4034573a2ee105c1d8
Rounak <riz.moj.msd97@gmail.com>
2022-07-19 23:32:08 +0530

    cors added

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 const port = process.env.PORT || 2345;
 const connect = require("./config/db")
 const short = require("./routes/urlShortener")
+var cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json());
 
 
