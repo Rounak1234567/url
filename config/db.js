1697bbf254820bff892dc1f0961c90063a668368
Rounak <riz.moj.msd97@gmail.com>
2022-12-28 11:43:27 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 
 const connect = async ()=>{
-    await mongoose.connect("mongodb+srv://rounak:<password>@urlshortener.8fq77ab.mongodb.net/?retryWrites=true&w=majority")
+    await mongoose.connect("mongodb+srv://rounak:rounakmojumder@urlshortener.8fq77ab.mongodb.net/?retryWrites=true&w=majority")
 
     console.log("connected to db")
 }
 
 module.exports = connect;
+
+//mongodb+srv://rounak:rounakmojumder@urlshortener.8fq77ab.mongodb.net/?retryWrites=true&w=majority
